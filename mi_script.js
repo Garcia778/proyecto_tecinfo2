bdde1f88102ce0cad7a373f3a8774f46ad66dc97
Garcia778 <A100245778@my.uvm.edu.mx>
2025-04-30 13:33:10 -0600

    mi_script.js

--- mi_script.js
+++ mi_script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const boton = document.getElementById("btnInteraccion");
+
     boton.addEventListener("click", function () {
-        alert("¡Gracias por visitar nuestro proyecto sobre el Robo de Información!");
+        alert("Gracias por visitar nuestro proyecto sobre el Robo de Información.");
     });
 });
